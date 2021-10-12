bb1cbed3b656de9ffd3add3d7ffefe93e5a31fd1
ranafaraznaru <ranafaraznaru@gmail.com>
2021-10-12 03:17:06 -0700

    nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -23,154 +23,128 @@ const useStyles = makeStyles({
     marginLeft: "38px !important",
   },
   contactButton: {
-    // borderRadius: "20px !important",
     marginLeft: "10px !important",
     color: "#0a2540 !important",
     fontWeight: "600 !important",
   },
   bgGradient: {
-    background:
-      "linear-gradient(150deg, #a960ee 15%, #ff333d 70%, #90e0ff 94%, #ffcb57 80%) !important",
-    // transform: "skewY(-12deg)",
-    transformOrigin: "0",
-    position: "absolute",
-    top: "0",
-    "&:span": {
-      height: "190px",
-      //  position: "absolute",
-    },
-    "&:span:nth-child(1)": {
-      width: "33.33%",
-      width: "calc(100%/3)",
-      left: "-16.66666%",
-      left: "calc(calc(calc(100%/3)/2)*-1)",
-    },
+    // background:
+    //   "linear-gradient(150deg, #a960ee 15%, #ff333d 70%, #90e0ff 94%, #ffcb57 80%) !important",
+    // width: "100%",
+    height: "100vh",
+    position: "relative",
+    // clipPath: "polygon(0% 0%, 100% 0%, 100% 30%, 0% 100%)",
   },
-  Nav: {
-    background:
-      "linear-gradient(150deg, #a960ee 15%, #ff333d 70%, #90e0ff 94%) !important",
-  },
+  // Nav: {
+  //   background:
+  //     "linear-gradient(150deg, #a960ee 15%, #ff333d 70%, #90e0ff 94%) !important",
+  // },
 });
 
 const Navbar = () => {
   const classes = useStyles();
   return (
-    <Grid container className={classes.bgGradient}>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-
-      <div style={{ width: "100%" }}>
-        <AppBar position="static" className={classes.Nav}>
-          <Toolbar>
-            <div style={{ width: "100%" }}>
-              <Box sx={{ display: "flex", p: 1 }}>
-                <Box sx={{ p: 1, flexGrow: 1 }}>
-                  {" "}
-                  <Typography
-                    variant="h3"
-                    component="div"
-                    sx={{
-                      font: "caption",
-                      fontWeight: "bold",
-                      fontSize: "27px",
-                    }}
-                  >
-                    Stripe
-                  </Typography>
+    <div container className={classes.bgGradient}>
+      <AppBar position="sticky" className={classes.Nav}>
+        <Toolbar>
+          <div style={{ width: "100%" }}>
+            <Box sx={{ display: "flex", p: 1 }}>
+              <Box sx={{ p: 1, flexGrow: 1 }}>
+                {" "}
+                <Typography
+                  variant="h3"
+                  component="div"
+                  sx={{
+                    font: "caption",
+                    fontWeight: "bold",
+                    fontSize: "27px",
+                  }}
+                >
+                  Stripe
+                </Typography>
+              </Box>
+              <Box sx={{ flexGrow: 1, display: "flex" }}>
+                <Box sx={{ pt: 2, px: 2, font: "caption", fontWeight: "bold" }}>
+                  Products
                 </Box>
-                <Box sx={{ flexGrow: 1, display: "flex" }}>
-                  <Box
-                    sx={{ pt: 2, px: 2, font: "caption", fontWeight: "bold" }}
-                  >
-                    Products
-                  </Box>
-                  <Box
-                    sx={{ pt: 2, px: 2, font: "caption", fontWeight: "bold" }}
-                  >
-                    Use cases
-                  </Box>
-                  <Box
-                    sx={{ pt: 2, px: 2, font: "caption", fontWeight: "bold" }}
-                  >
-                    Developers
-                  </Box>
-                  <Box
-                    sx={{ pt: 2, px: 2, font: "caption", fontWeight: "bold" }}
-                  >
-                    Company
-                  </Box>
-                  <Box
-                    sx={{ pt: 2, px: 2, font: "caption", fontWeight: "bold" }}
-                  >
-                    Pricing
-                  </Box>
+                <Box sx={{ pt: 2, px: 2, font: "caption", fontWeight: "bold" }}>
+                  Use cases
                 </Box>
-                <Box sx={{ p: 1 }}>
-                  {" "}
-                  <Button
-                    variant="contained"
-                    className={classes.signinButton}
-                    sx={{ display: "flex", justifyContent: "flex-end" }}
-                  >
-                    Sign in
-                    <ArrowForwardIos className={classes.ArrowSize} />
-                  </Button>
+                <Box sx={{ pt: 2, px: 2, font: "caption", fontWeight: "bold" }}>
+                  Developers
+                </Box>
+                <Box sx={{ pt: 2, px: 2, font: "caption", fontWeight: "bold" }}>
+                  Company
+                </Box>
+                <Box sx={{ pt: 2, px: 2, font: "caption", fontWeight: "bold" }}>
+                  Pricing
                 </Box>
               </Box>
-            </div>
-          </Toolbar>
-        </AppBar>
-      </div>
-      <Grid item xs={12} md={7}>
-        <div>
-          <Typography
-            variant="h1"
-            component="div"
-            gutterBottom
-            pl={8}
-            pt={16}
-            sx={{ text: "bold", fontWeight: "600" }}
-          >
-            Payments infrastructure for the internet
-          </Typography>
-          <Typography
-            variant="h6"
-            pl={8}
-            sx={{ color: "#85919d", fontWeight: "400", lineHeight: "1.3" }}
-          >
-            Millions of companies of all sizes—from startups to Fortune
-            <br /> 500s—use Stripe’s software and APIs to accept payments,
-            <br /> send payouts, and manage their businesses online.
-          </Typography>
-          <Toolbar sx={{ marginTop: "20px" }}>
-            <Button variant="contained" className={classes.startnowButton}>
-              Start now
-              <ArrowForwardIos className={classes.ArrowSize} />
-            </Button>
-            <Button className={classes.contactButton}>
-              Contact sales
-              <ArrowForwardIos className={classes.ArrowSize} />
-            </Button>
-          </Toolbar>
-        </div>
-      </Grid>
-
-      <Grid item xs={12} md={5} pt={4}>
-        <Toolbar>
-          <Grid item xs={6} pt={5}>
-            <img src={mob1} />
-          </Grid>
-
-          <Grid item xs={6}>
-            <img src={mob2} className={classes.mob2Img} />
-          </Grid>
+              <Box sx={{ p: 1 }}>
+                {" "}
+                <Button
+                  variant="contained"
+                  className={classes.signinButton}
+                  sx={{ display: "flex", justifyContent: "flex-end" }}
+                >
+                  Sign in
+                  <ArrowForwardIos className={classes.ArrowSize} />
+                </Button>
+              </Box>
+            </Box>
+          </div>
         </Toolbar>
+      </AppBar>
+
+      <Grid container>
+        <Grid item xs={12} md={7}>
+          <div>
+            <Typography
+              variant="h1"
+              component="div"
+              gutterBottom
+              pl={8}
+              pt={16}
+              sx={{ text: "bold", fontWeight: "600" }}
+            >
+              Payments infrastructure for the internet
+            </Typography>
+            <Typography
+              variant="h6"
+              pl={8}
+              sx={{ color: "#85919d", fontWeight: "400", lineHeight: "1.3" }}
+            >
+              Millions of companies of all sizes—from startups to Fortune
+              <br /> 500s—use Stripe’s software and APIs to accept payments,
+              <br /> send payouts, and manage their businesses online.
+            </Typography>
+            <Toolbar sx={{ marginTop: "20px" }}>
+              <Button variant="contained" className={classes.startnowButton}>
+                Start now
+                <ArrowForwardIos className={classes.ArrowSize} />
+              </Button>
+              <Button className={classes.contactButton}>
+                Contact sales
+                <ArrowForwardIos className={classes.ArrowSize} />
+              </Button>
+            </Toolbar>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={5} pt={4}>
+          <Toolbar>
+            <Grid item xs={6} pt={5}>
+              <img src={mob1} />
+            </Grid>
+
+            <Grid item xs={6}>
+              <img src={mob2} className={classes.mob2Img} />
+            </Grid>
+          </Toolbar>
+        </Grid>{" "}
       </Grid>
-    </Grid>
+    </div>
   );
 };
 export default Navbar;
 // "start": "react-scripts start",
+// "start": "webpack-dev-server --env.ENVIRONMENT=development --content-base src/ --mode development --watch --watch-poll",
