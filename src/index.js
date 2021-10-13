7d4106dfa5cf4117a3ce4b9cb8a3f7af0ffb927e
ranafaraznaru <ranafaraznaru@gmail.com>
2021-10-12 23:16:07 -0700

    pay

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Navbar from "./Navbar";
 import OurClients from "./OurClients";
+import PaymentsProducts from "./PaymentsProducts";
 const App = () => {
   return (
     <>
       <Navbar />
       <OurClients />
+      <PaymentsProducts />
     </>
   );
 };
